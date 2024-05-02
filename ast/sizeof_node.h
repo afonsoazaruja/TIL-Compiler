df255a3761b478af722e067c29ff3f607c372b95
Afonso Azaruja <afonso.azaruja@tecnico.ulisboa.pt>
2024-05-02 19:36:37 +0100

    fix: Sizeof node.

--- ast/sizeof_node.h
+++ ast/sizeof_node.h
@@ -6,14 +6,10 @@
 namespace til {
 
   class sizeof_node : public cdk::unary_operation_node {
-    cdk::expression_node *_expression;
+
   public:
     inline sizeof_node(int lineno, cdk::expression_node *argument)
-        : cdk::unary_operation_node(lineno, argument), _expression(argument) {}
-
-    cdk::expression_node* expression() {
-      return _expression;
-    }
+        : cdk::unary_operation_node(lineno, argument) {}
 
     void accept(basic_ast_visitor *sp, int level) {
       sp->do_sizeof_node(this, level);
